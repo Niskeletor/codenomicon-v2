d7156cb595d40b96185e1003cffa148bb6bfa1db
Niskeletor <niskeletor@gmail.com>
2024-04-22 16:45:41 +0200

    arreglos menores

--- src/components/CardsDisplay.jsx
+++ src/components/CardsDisplay.jsx
@@ -11,7 +11,7 @@ function CardDatos() {
       {datos.map((dato, index) => (
         <div key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <svg className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            {/* Aquí puedes reemplazar el SVG con una imagen o ícono relacionado con tus datos */}
+            {/* Imagen SVG */}
           </svg>
           <a href={dato.url}>
             <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{dato.titulo}</h5>
