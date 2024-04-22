18397836667cf2092981b08491ac80b0918488b4
Niskeletor <niskeletor@gmail.com>
2024-04-22 22:38:46 +0200

    control si no existe imagen, se sustituye por el logo de la pagina

--- src/components/CardDatos.jsx
+++ src/components/CardDatos.jsx
@@ -10,20 +10,24 @@ function CardDatos() {
       {datos.map((dato, index) => (
         <div key={index} className="block rounded-lg bg-white shadow-lg dark:bg-neutral-700 text-center">
          
-  
+  {/* Card image */}
   <a href="#!">
-    <img class="rounded-t-lg" src="../../public/codenomicon_icon.png" alt="" />
+    <img class="rounded-t-lg" src={
+      dato.img && dato.img !== "Failed to retrieve" && dato.img !== "Not Found" && dato.img !== "og-image.png"
+      ? dato.img
+      : "/codenomicon_icon.png"
+    } alt="" />
   </a>
 
-  
+  {/* Card body */}
   <div class="p-6">
 
-    
+    {/* Title */}
     <h5 class="mb-2 text-xl font-bold tracking-wide text-neutral-800 dark:text-neutral-50">
       {dato.titulo}
     </h5>
 
-    
+    {/* Text */}
     <p class="mb-2 text-base text-neutral-500 dark:text-neutral-300">
       {dato.descripcion}
     </p>
